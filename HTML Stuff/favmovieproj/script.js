755bc324050c43cf03b45b0c4783fef0fe760701
N3r4V <132166165+N3r4V@users.noreply.github.com>
2025-08-25 03:32:51 +0800

    Add files via upload

---
+++ HTML Stuff/favmovieproj/script.js
@@ -0,0 +1,32 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove("active");
+        if (i === index) slide.classList.add("active");
+    });
+    currentSlide = index;
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+function goToSlide(index) {
+     showSlide(index);
+}
+
+function toggleDescription(num) {
+      const desc = document.getElementById("desc" + num);
+      desc.style.display = desc.style.display === "block" ? "none" : "block";
+}
+
+// Init first slide
+showSlide(0);
